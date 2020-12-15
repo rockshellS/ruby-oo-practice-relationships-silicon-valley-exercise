32f8a08a77e474e17e48a886580c6225c8616db2
rockshellS <spicer.rachelle@gmail.com>
2020-12-14 20:47:18 -0600

    Done.

--- app/models/venture_capitalist.rb
+++ app/models/venture_capitalist.rb
@@ -10,17 +10,46 @@ class VentureCapitalist
 
    end
 
+   def self.all
+      @@all
+     end
 
 def self.tres_commas_club
     self.all.find_all {|v| v.total_worth > 1000000000}
 
 end
 
+def offer_contract(startup, type, investment)
+   FundingRound.new(startup, self, type, investment)
+end
+
+def funding_rounds
+   FundingRound.all.select {|fr| fr.venturecapitalist == self}
+end
+
+def portfolio
+   funding_rounds.collect {|fr| fr.startup}.uniq
+end 
 
 
-   def self.all
-    @@all
-   end
+def biggest_investment
+   funding_rounds.max_by {|fr| fr.investment}
+end 
+
+
+def invested(domainstring)
+   amount_invested = 0
+   funding_rounds.collect do |fr|
+       if fr.startup.domain == domainstring
+           amount_invested += fr.investment
+       end
+   end  
+   amount_invested
+end 
+end
+
+
+   
 
 
 end
